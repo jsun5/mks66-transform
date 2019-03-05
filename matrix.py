05dd0372babfbef3df397fe79d4416117c5d1417
jsun5 <jsun5@stuy.edu>
2019-03-04 20:55:21 -0500

    cleaning up

--- matrix.py
+++ matrix.py
@@ -11,10 +11,7 @@ import math
 
 def make_translate( x, y, z ):
     m = new_matrix()
-    m[0][0]=1
-    m[1][1]=1
-    m[2][2]=1
-    m[3][3]=1
+    ident(m)
     m[3][0]=x
     m[3][1]=y
     m[3][2]=z
@@ -22,44 +19,41 @@ def make_translate( x, y, z ):
 
 def make_scale( x, y, z ):
     m = new_matrix()
+	ident(m)
     m[0][0]=x
     m[1][1]=y
     m[2][2]=z
-    m[3][3]=1
     return m
 
-def make_rotX( theta ): 
+def make_rotY( theta ): 
     theta = math.radians(theta)
     m = new_matrix()
-    m[0][0]=math.cos(theta)
-    m[0][2]=-math.sin(theta)
-    m[1][1]=1
-    m[2][0]=math.sin(theta)
-    m[2][2]=math.cos(theta)
-    m[3][3]=1
+	ident(m)
+    m[0][0]= math.cos(theta)
+    m[0][2]= -math.sin(theta)
+    m[2][0]= math.sin(theta)
+    m[2][2]= math.cos(theta)
     return m
     
 
-def make_rotY( theta ):
+def make_rotX( theta ):
     theta = math.radians(theta)
     m = new_matrix()
-    m[0][0]=1
-    m[1][1]=math.cos(theta)
-    m[1][2]=math.sin(theta)
-    m[2][1]=-math.sin(theta)
-    m[2][2]=math.cos(theta)
-    m[3][3]=1
+    ident(m)
+    m[1][1]= math.cos(theta)
+    m[1][2]= math.sin(theta)
+    m[2][1]= -math.sin(theta)
+    m[2][2]= math.cos(theta)
     return m
 
 def make_rotZ( theta ):
     theta = math.radians(theta)
     m = new_matrix()
-    m[0][0]=math.cos(theta)
-    m[0][1]=math.sin(theta)
-    m[1][0]=-math.sin(theta)
-    m[1][1]=math.cos(theta)
-    m[2][2]=1
-    m[3][3]=1
+	ident(m)
+    m[0][0]= math.cos(theta)
+    m[0][1]= math.sin(theta)
+    m[1][0]= -math.sin(theta)
+    m[1][1]= math.cos(theta)
     return m
 
 #print the matrix such that it looks like
